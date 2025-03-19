04dc302f4f8de1afb3afe013fdae71f9e56eb6d0
TESF 度汶 <3257600966@qq.com>
2025-03-19 21:37:22 +0800

    4驱变2驱（悲

--- inc/car.h
+++ inc/car.h
@@ -7,9 +7,7 @@ sbit TR20 = P1^1;
 sbit TR30 = P1^2;
 sbit TR40 = P1^3;    //循迹模块
 sbit EN1 = P1^4;
-sbit EN2 = P1^5;
-sbit EN3 = P1^6;
-sbit EN4 = P1^7;    //电机使能
+sbit EN2 = P1^5;    //电机使能
 sbit ST0 = P2^7;    //避障模块
 
 #endif
