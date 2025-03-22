e7682d1f501f718ce09b8801d21688782080e51b
TESF 度汶 <3257600966@qq.com>
2025-03-23 01:39:50 +0800

    改错别字

--- inc/auto_ctrl.h
+++ inc/auto_ctrl.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_CTRL_H__
 #define __AUTO_CTRL_H__
 void auto_control();    //定义自动控制函数
-extern bit end_flag;    //定义结束标
+extern bit end_flag;    //定义结束标志
 #endif
--- inc/pwm.h
+++ inc/pwm.h
@@ -3,7 +3,7 @@
 extern unsigned char compare;           //PWM程序内部比较值
 extern unsigned char speed1,speed2;     //speed1左侧两轮速度,speed2右侧两轮速度
 void Timer0_Init(void);                 //初始化定时器0
-extern bit is_turning;                  //定义即使标志
+extern bit is_turning;                  //定义计时标志
 extern unsigned char turn_time;         //时间
 extern long runing_time;                 //运行时间
 #endif
